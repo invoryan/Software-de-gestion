5fab5e3c396f1391260c8a2e25ffe57dc7a77996
Bryan Hurtado Delgado <bryanhurtadodelgado@gmail.com>
2023-01-05 13:03:27 -0500

    se añadio la plataforma para el administrador y para el empleado

--- src/com/unamba/cinemagicoabancay/main/main.java
+++ src/com/unamba/cinemagicoabancay/main/main.java
@@ -1,11 +1,10 @@
 package com.unamba.cinemagicoabancay.main;
-import com.unamba.cinemagicoabancay.body.homeAdmin;
-
+import com.unamba.cinemagicoabancay.login.login;
 public class main {
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new homeAdmin().setVisible(true);
+                new login().setVisible(true);
             }
         });
     } 
